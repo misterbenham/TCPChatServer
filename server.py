26e3ebfde721e8236483f80a4c9830de97750042
Harry Perry Benham <harry.perrybenham@bt.com>
2022-08-30 14:33:08 +0100

    19-basic-server-functionality

---
+++ server.py
@@ -0,0 +1,40 @@
+import logging
+import socket
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+
+class Server:
+    """
+    Supports management of server connections.
+    Attributes:
+        host (str): The IP address of the listening socket.
+        port (int): The port number of the listening socket.
+    """
+
+    def __init__(self, host, port):
+        super().__init__()
+        self.host = host
+        self.port = port
+
+    def run(self):
+        """
+        Creates the listening socket.
+        Binds server to host IP and port.
+        """
+        logging.info("Creating socket...")
+        ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info("Socket created")
+        try:
+            logging.debug(f"Binding server socket to {self.host} : {self.port}...")
+            ServerSocket.bind((self.host, self.port))
+            logging.info(f"Server socket bound to {self.host} : {self.port}")
+        except socket.error as e:
+            logging.info(e)
+        logging.info(f"Server is listening on port {self.port}...")
+        ServerSocket.listen()
+
+
+if __name__ == '__main__':
+    server = Server('127.0.0.1', 5555)
+    server.run()
