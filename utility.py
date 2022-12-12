7818a2980665063392ceaed410776a539a549ffc
Harry Perry Benham <harry.perrybenham@bt.com>
2022-12-12 13:02:27 +0000

    57-refactor enums

--- utility.py
+++ utility.py
@@ -6,48 +6,48 @@ class LoginCommands(enum.Enum):
     Enum Header values to be used for json packets.
     Values for login commands.
     """
-    LOGIN = 'login'
-    REGISTER = 'register'
-    REGISTERED = 'registered'
-    LOGGED_IN = 'logged_in'
+    LOGIN = '1'
+    REGISTER = '2'
+    REGISTERED = '3'
+    LOGGED_IN = '4'
 
 
 class LoggedInCommands(enum.Enum):
     """
     Header values for logged in commands.
     """
-    BROADCAST = 'broadcast'
-    AUTHENTICATE_DIRECT_MESSAGE = 'dm'
-    DIRECT_MESSAGE = 'direct_message'
-    PRINT_DM = 'print_dm'
-    ADD_FRIEND = 'af'
-    VIEW_FRIEND_REQUESTS = 'fr'
-    VIEW_FRIENDS = 'vf'
-    AUTH_TIC_TAC_TOE = 'ttt'
-    VIEW_TIC_TAC_TOE_REQUESTS = 'vttt'
-    SET_STATUS_AWAY = 'ssa'
-    HELP = 'help'
-    QUIT = 'quit'
+    BROADCAST = '1'
+    AUTHENTICATE_DIRECT_MESSAGE = '2'
+    DIRECT_MESSAGE = '3'
+    PRINT_DM = '4'
+    ADD_FRIEND = '5'
+    VIEW_FRIEND_REQUESTS = '6'
+    VIEW_FRIENDS = '7'
+    AUTH_TIC_TAC_TOE = '8'
+    VIEW_TIC_TAC_TOE_REQUESTS = '9'
+    SET_STATUS_AWAY = '10'
+    HELP = '11'
+    QUIT = '12'
 
 
 class Responses(enum.Enum):
     """
     Header values for responses.
     """
-    SUCCESS = 'success'
-    ERROR = 'error'
-    BROADCAST_MSG = 'broadcast_msg'
-    DM_ERROR = 'dme'
-    PRINT_FRIEND_REQUESTS = 'pfr'
-    PRINT_FRIENDS_LIST = 'pfl'
-    PRINT_STATUS_AWAY = 'ssa'
-    PRINT_TTT_REQUESTS = 'ptr'
-    ONLINE_NOTIFICATION = 'of'
-    TIC_TAC_TOE_REQUEST = 'tttr'
-    TIC_TAC_TOE_CONFIRM = 'tttc'
-    TIC_TAC_TOE_DENY = 'tttd'
-    PLAY_TIC_TAC_TOE = 'pttt'
-    TIC_TAC_TOE_ERROR = 'ttte'
-    TIC_TAC_TOE_SPACE_ERROR = 'Space already filled!'
-    TIC_TAC_TOE_WINNER = 'tttwin'
-    TIC_TAC_TOE_TIE = 'tttt'
+    SUCCESS = '1'
+    ERROR = '2'
+    BROADCAST_MSG = '3'
+    DM_ERROR = '4'
+    PRINT_FRIEND_REQUESTS = '5'
+    PRINT_FRIENDS_LIST = '6'
+    PRINT_STATUS_AWAY = '7'
+    PRINT_TTT_REQUESTS = '8'
+    ONLINE_NOTIFICATION = '9'
+    TIC_TAC_TOE_REQUEST = '10'
+    TIC_TAC_TOE_CONFIRM = '11'
+    TIC_TAC_TOE_DENY = '12'
+    PLAY_TIC_TAC_TOE = '13'
+    TIC_TAC_TOE_ERROR = '14'
+    TIC_TAC_TOE_SPACE_ERROR = '15'
+    TIC_TAC_TOE_WINNER = '16'
+    TIC_TAC_TOE_TIE = '17'
