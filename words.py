bcb275e40cf36458905c4a2234e7cd8c2a97bbad
Harry Perry Benham <harry.perrybenham@bt.com>
2022-12-13 13:09:27 +0000

    59-wordle random word generator
    - all 5 letter words now written to txt file
    - function reads the text file and returns a random 5 letter string

--- words.py
+++ words.py
@@ -1,20 +1,25 @@
 import random
 import requests
 
+WORD_SITE = "https://www.mit.edu/~ecprice/wordlist.10000"
+
 
 def get_word():
-    word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-    response = requests.get(word_site)
+    response = requests.get(WORD_SITE)
     word_list = response.content.splitlines()
     output_words_list = []
 
-    for i in range(len(word_list)-1):
-        selected = word_list[i].decode('utf-8')
+    for word in word_list:
+        selected = word.decode('utf-8')
 
-        if "".join(dict.fromkeys(selected)) == selected and len(selected) == 5:
-
+        if len(selected) == 5:
             output_words_list.append(selected)
 
-    chosen_word = random.choice(output_words_list)
-    print(chosen_word)
-    return chosen_word
+    with open("words.txt", "r") as wf:
+        output_words_list = wf.readlines()
+        chosen_word = random.choice(output_words_list)
+        print(chosen_word)
+        return chosen_word
+
+
+get_word()
