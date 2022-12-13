91684d9b4b07b255bcf430596bf88b4527b65d9a
Harry Perry Benham <harry.perrybenham@bt.com>
2022-12-13 13:47:57 +0000

    59-wordle random word generator V3
    - file is validated if already exists

--- words.py
+++ words.py
@@ -1,3 +1,4 @@
+import os.path
 import random
 import requests
 
@@ -5,21 +6,23 @@ WORD_SITE = "https://www.mit.edu/~ecprice/wordlist.10000"
 
 
 def get_word():
-    response = requests.get(WORD_SITE)
-    word_list = response.content.splitlines()
-    output_words_list = []
+    is_file = os.path.isfile('words.txt')
+    if is_file:
+        with open("words.txt", "r") as wf:
+            output_words_list = wf.readlines()
+            chosen_word = random.choice(output_words_list)
+            print(chosen_word)
+            return chosen_word
+    else:
+        response = requests.get(WORD_SITE)
+        word_list = response.content.splitlines()
+        output_words_list = []
 
-    for word in word_list:
-        selected = word.decode('utf-8')
+        for word in word_list:
+            selected = word.decode('utf-8')
 
-        if len(selected) == 5:
-            output_words_list.append(selected)
-
-    with open("words.txt", "r") as wf:
-        output_words_list = wf.readlines()
-        chosen_word = random.choice(output_words_list)
-        print(chosen_word)
-        return chosen_word
+            if len(selected) == 5:
+                output_words_list.append(selected)
 
 
 get_word()
