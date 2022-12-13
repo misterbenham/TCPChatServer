2430a095b644112f9f5f05e6225c99a3dd7fa757
Harry Perry Benham <harry.perrybenham@bt.com>
2022-12-13 14:29:38 +0000

    59-wordle random word generator V4
    - if the words.txt file exists, the function reads from and returns random word
    - if file does not exist, function creates file, writes to and returns random string

--- words.py
+++ words.py
@@ -8,21 +8,26 @@ WORD_SITE = "https://www.mit.edu/~ecprice/wordlist.10000"
 def get_word():
     is_file = os.path.isfile('words.txt')
     if is_file:
-        with open("words.txt", "r") as wf:
-            output_words_list = wf.readlines()
+        with open("words.txt", "r") as rf:
+            output_words_list = rf.readlines()
             chosen_word = random.choice(output_words_list)
             print(chosen_word)
             return chosen_word
     else:
-        response = requests.get(WORD_SITE)
-        word_list = response.content.splitlines()
-        output_words_list = []
+        with open("words.txt", "w") as wf:
+            response = requests.get(WORD_SITE)
+            word_list = response.content.splitlines()
+            output_words_list = []
 
-        for word in word_list:
-            selected = word.decode('utf-8')
+            for word in word_list:
+                selected = word.decode('utf-8')
 
-            if len(selected) == 5:
-                output_words_list.append(selected)
+                if len(selected) == 5:
+                    output_words_list.append(selected)
+
+            for line in output_words_list:
+                wf.writelines(f"{line}\n")
+    get_word()
 
 
 get_word()
